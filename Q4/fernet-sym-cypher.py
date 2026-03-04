b9927727a52678b764457348fc5d649972595b46
Joana Silva <josilva@uevora.pt>
2026-03-04 10:16:52 +0000

    ex4 (wip)

---
+++ Q4/fernet-sym-cypher.py
@@ -0,0 +1,18 @@
+from cryptography.fernet import Fernet
+
+key = Fernet.generate_key()
+# the key is type bytes
+print("key: ", key)
+
+f = Fernet(key)
+# encrypt the message
+token = f.encrypt(b"my deep dark secret")
+print("token: ", token)
+
+# decrypt the message
+msg = f.decrypt(token)
+print("plain text", msg)
+
+filename = 'key.fernet'
+with open(filename, 'wb') as file:
+    file.write(key)
